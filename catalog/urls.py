4327cfeb7fe919d08ec9fac689febd81ca3fe4a9
tolji <toljik1@mail.ru>
2024-07-21 22:15:29 +0400

    add static, media, inc_menu, products_detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import product_list, contacts, products_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('', product_list, name='product_list'),
+    path('contacts/', contacts, name='contacts'),
+    path('products/<int:pk>/', products_detail, name='product_detail')
 ]
